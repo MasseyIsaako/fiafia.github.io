3114a00ad55b8362d0b4b8d0ae96cc4c02e12fc6
MasseyIsaako <misaako@squiz.net>
2021-09-03 08:57:39 +1000

    master: added basic item card styles updated profiles.

--- src/modules/ItemCards/ItemCards.jsx
+++ src/modules/ItemCards/ItemCards.jsx
@@ -19,8 +19,17 @@ class ItemCards extends React.Component {
 
             this.state.cards.forEach((card, index) => {
                 cards.push(
-                    <Col key={index}>
-                        <h2 dangerouslySetInnerHTML={{ __html: translator(card.heading) }}></h2>
+                    <Col key={index} md={6} className="pb-3">
+                        <div className="item-card">
+                            <h2 className="item-card__heading">
+                                <span className="english">{card.previewHeading.en}</span>
+                                <span className="samoan">{card.previewHeading.sm}</span>
+                            </h2>
+                            <div className="item-card__image"
+                                style={{ backgroundImage: `url('https://images.unsplash.com/photo-1515249094873-c9763a456e05?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80')` }}>
+                                <span className="item-card__number">{index + 1}</span>
+                            </div>
+                        </div>
                     </Col>
                 );
             });
@@ -38,11 +47,11 @@ class ItemCards extends React.Component {
 
         return (
             cards &&
-            <section>
+            <section className="item-cards">
                 <Container>
                     <Row>
                         <Col sm={12}>
-                            <h1>{this.state.heading}</h1>
+                            <h1 className="item-cards__heading">{this.state.heading}</h1>
                         </Col>
                     </Row>
                     <Row>
